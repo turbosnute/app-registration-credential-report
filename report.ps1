2721a88fc4126a4a61ea341f302ceb28e9d6746c
Øyvind Andreas Nilsen <on@ntnu.no>
2023-12-28 12:01:47 +0100

    var init, function and script part

--- report.ps1
+++ report.ps1
@@ -1,33 +1,14 @@
 
+#
+# Variable initializations
+#
 
 $uri = 'https://graph.microsoft.com/v1.0/applications?$select=id,appId,displayName,description,keyCredentials'
 $ownersUri = 'https://graph.microsoft.com/v1.0/applications/{id}/owners?$select=id,displayName,mail,userPrincipalName'
 
-Write-Host "Getting all app registrations that have key credentials..."
-
-$all_apps = do {
-    $res = Invoke-MgGraphRequest -Uri $uri
-    $res.value
-    $URI = $res.'@odata.nextLink'
-} while ($res.'@odata.nextLink')
-
-Write-Host -f Cyan "[Done]" -NoNewline; Write-Host " Found $($all_apps.count) app registrations."
-
-$apps_with_creds = $all_apps | Where-Object { $_.keyCredentials } # Unfortunately keyCredentials kan not be filtered in the Graph Uri.
-
-Write-Host "$($apps_with_creds.count) of the apps got key credentials..."
-Write-Host "Checking status and getting owners..."
-
-foreach ($app in $apps_with_creds) {
-
-        # Get and add owners ...
-        $appOwnersUri = $ownersUri -replace '{id}', $app.id
-        $appOwners = (Invoke-MgGraphRequest -Uri $appOwnersUri).Value
-        $app['Owners'] = $appOwners
-        
-        # Foreach credential
-        # ...
-}
+#
+# Functions 
+#
 
 
 <#
@@ -58,3 +39,36 @@ function Get-TickTickPercent {
         $percentage
     }
 }
+
+#
+# Script
+#
+
+Write-Host "Getting all app registrations that have key credentials..."
+
+$all_apps = do {
+    $res = Invoke-MgGraphRequest -Uri $uri
+    $res.value
+    $URI = $res.'@odata.nextLink'
+} while ($res.'@odata.nextLink')
+
+Write-Host -f Cyan "[Done]" -NoNewline; Write-Host " Found $($all_apps.count) app registrations."
+
+$apps_with_creds = $all_apps | Where-Object { $_.keyCredentials } # Unfortunately keyCredentials kan not be filtered in the Graph Uri.
+
+Write-Host "$($apps_with_creds.count) of the apps got key credentials..."
+Write-Host "Checking status and getting owners..."
+
+foreach ($app in $apps_with_creds) {
+
+        # Get and add owners ...
+        $appOwnersUri = $ownersUri -replace '{id}', $app.id
+        $appOwners = (Invoke-MgGraphRequest -Uri $appOwnersUri).Value
+        $app['Owners'] = $appOwners
+        
+        # Foreach credential
+        foreach ($cert in $app.keyCredentials) {
+            # ...
+        }
+}
+
