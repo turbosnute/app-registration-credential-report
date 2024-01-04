672784d421abb1fee0337e9fab6a833eda31366b
Øyvind Andreas Nilsen <on@ntnu.no>
2024-01-04 11:20:13 +0100

    script works now

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # EntraID App Registration Credential Report
-This script is used to make a report of certificates used for app registration authentication in Entra ID.
+This script is used to make a quick and dirty report of certificates and client secrets used for app registration authentication in Entra ID.
+
+# Reqiured Powershell Module
+```
+Microsoft.Graph.Authentication
+```
+# Required Graph API Permissions
+```
+Application.Read.All
+```
+
+# Usage
+```
+.\report.ps1 -FilePath "app-registration-report.html"
+```
--- report.ps1
+++ report.ps1
@@ -1,9 +1,14 @@
+param(
+    [Parameter(Mandatory=$true)][string]$FilePath
+)
+
+Connect-MgGraph
 
 #
 # Variable initializations
 #
 
-$uri = 'https://graph.microsoft.com/v1.0/applications?$select=id,appId,displayName,description,keyCredentials'
+$uri = 'https://graph.microsoft.com/v1.0/applications?$select=id,appId,displayName,description,keyCredentials,passwordCredentials'
 $ownersUri = 'https://graph.microsoft.com/v1.0/applications/{id}/owners?$select=id,displayName,mail,userPrincipalName'
 
 #
@@ -40,39 +45,12 @@ function Get-TickTickPercent {
     }
 }
 
-<#
-.SYNOPSIS
-    Short description
-.DESCRIPTION
-    Long description
-.EXAMPLE
-    Example of how to use this cmdlet
-.EXAMPLE
-    Another example of how to use this cmdlet
-#>
-function Get-ProgressBarBox {
-    [CmdletBinding()]
-    [OutputType([int])]
-    param(
-        [Parameter(Mandatory=$true)][string]$Param1
-    )
-    
-    begin {
-    }
-    
-    process {
-        
-    }
-    
-    end {
-    }
-}
 
 <#
 .SYNOPSIS
-    Short description
+    Exports a html report of the app registrations.
 .DESCRIPTION
-    Long description
+    Exports a html report of the app registrations.
 #>
 function Export-AppRegistrationCertificateReport {
     [CmdletBinding()]
@@ -91,7 +69,9 @@ function Export-AppRegistrationCertificateReport {
         $html.Add('<title>App Registration Report</title>')
         $html.Add('<style>')
         #$html.Add('progress { background-color: #f3f3f3; border: 0; height: 18px; border-radius: 9px; }')
-        $html.Add('table { background-color:#ffd899; padding: 5px; margin: 5px; }')
+        $html.Add('table { padding: 5px; margin: 5px; }')
+        $html.Add('.cert { background-color:#ffd899; }')
+        $html.Add('.sec { background-color:#f0b6b6; }')
         $html.Add('body { font-family: Arial, Helvetica, sans-serif; }')
         $html.Add('</style>')
         $html.Add('<body>')
@@ -103,10 +83,36 @@ function Export-AppRegistrationCertificateReport {
             if ($null -ne $app.decription -and $app.description -ne "") {
                 $html.Add("<strong>description: </strong>$($app.description)<br />")
             }
+            if ($null -ne $app.Owners.mail -and $app.Owners.mail -ne "") {
+                $ownerlist = New-Object System.Collections.Generic.List[System.String]
+                foreach ($Owner in $app.Owners) {
+                    $ownerlist.Add("<a href='mailto:$($Owner.mail)'>$($Owner.displayName)</a>")
+                }
+                $owenerlistString = $ownerlist -join ', '
+                $html.Add("<strong>Owners: </strong>$owenerlistString<br />")
+            }
             $html.Add('</p>')
+            # Client Secrets
+            foreach ($sec in $app.passwordCredentials) {
+                $html.Add('<table class="sec">')
+                $html.Add("<tr><th colspan='2'>Client Secret</th></tr>")
+                $html.Add("<tr><th>Name</th><td>$($sec.displayName)</td></tr>")
+                $html.Add("<tr><th>KeyId</th><td>$($sec.keyId)</td></tr>")
+                if ($sec.Expired) {
+                    $html.Add("<tr><th>Valid until</th><td><span style=`"color:red; font-weight: bold;`">$($sec.endDateTime.toString('yyyy-MM-dd HH:mm:ss'))</span></td></tr>")
+                } else {
+                    $html.Add("<tr><th>Valid until</th><td>$($sec.endDateTime.toString('yyyy-MM-dd HH:mm:ss'))</td></tr>")
+                }
+                $Percentage = $sec.Percentage
+                $certlifetime = "<progress value='$Percentage' max='100'></progress>"
+                $html.Add("<tr><th>Lifetime</th><td>$certlifetime</td></tr>")
+                $html.Add('</table>')
+            }
 
+            # Certificates
             foreach ($key in $app.keyCredentials) {
-                $html.Add('<table>')
+                $html.Add('<table class="cert">')
+                $html.Add("<tr><th colspan='2'>Certificate</th></tr>")
                 $html.Add("<tr><th>Name</th><td>$($key.displayName)</td></tr>")
                 $html.Add("<tr><th>KeyId</th><td>$($key.keyId)</td></tr>")
                 if ($key.Expired) {
@@ -143,9 +149,9 @@ $all_apps = do {
 
 Write-Host -f Cyan "[Done]" -NoNewline; Write-Host " Found $($all_apps.count) app registrations."
 
-$apps_with_creds = $all_apps | Where-Object { $_.keyCredentials } # Unfortunately keyCredentials kan not be filtered in the Graph Uri.
+$apps_with_creds = $all_apps | Where-Object { $_.keyCredentials -or $_.passwordCredentials } # Unfortunately keyCredentials kan not be filtered in the Graph Uri.
 
-Write-Host "$($apps_with_creds.count) of the apps got key credentials..."
+Write-Host "$($apps_with_creds.count) of the apps got certificates or client secrets..."
 Write-Host "Checking status and getting owners..."
 
 foreach ($app in $apps_with_creds) {
@@ -155,7 +161,14 @@ foreach ($app in $apps_with_creds) {
         $appOwners = (Invoke-MgGraphRequest -Uri $appOwnersUri).Value
         $app['Owners'] = $appOwners
         
-        # Foreach credential
+        # Foreach client secret
+        foreach ($sec in $app.passwordCredentials) {
+            $sec['Expired'] = [bool]((Get-Date) -gt $sec.endDateTime)
+            $percentage = Get-TickTickPercent -startDateTime $sec.startDateTime -endDateTime $sec.endDateTime
+            $sec['Percentage'] = $percentage
+        }
+
+        # Foreach certificate credential
         foreach ($cert in $app.keyCredentials) {
             $cert['Expired'] = [bool]((Get-Date) -gt $cert.endDateTime)
             
@@ -164,3 +177,6 @@ foreach ($app in $apps_with_creds) {
         }
 }
 
+
+Export-AppRegistrationCertificateReport -apps_with_creds $apps_with_creds | Out-File -FilePath $FilePath -Encoding utf8; 
+Write-Host "Saved to '$FilePath'"
